75b2c104c591b62519ea7a03ca07cfa7aa2f2527
lhien11@gmail.com <trunghien11>
2018-03-21 10:16:45 -0700

    image work but need work on displaying

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  PIXABAY_API_URL: "https://pixabay.com/api/?key=8435589-9c1187cdd029516aaff276079",
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: true,
+    PIXABAY_API_URL: "https://pixabay.com/api/?key=8435589-9c1187cdd029516aaff276079",
+  };
+  
